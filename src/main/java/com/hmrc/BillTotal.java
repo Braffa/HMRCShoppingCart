168a569c115ad884a35331aaf4c5e98f95c2e35d
Braffa <dave.brayfield@gmail.com>
2018-10-16 08:57:45 +0100

    moved discount processing to lambda functions

--- src/main/java/com/hmrc/BillTotal.java
+++ src/main/java/com/hmrc/BillTotal.java
@@ -6,6 +6,29 @@ public class BillTotal {
 
 	private static double applePrice = 0.60;
 	private static double orangePrice = 0.25;
+	
+	Fruit apples = (int totalApples) -> {
+		if (totalApples > 0) {
+			if (totalApples % 2 == 0) {
+				totalApples = totalApples / 2;
+			} else {
+				totalApples = (totalApples / 2) + 1;
+			}
+		}
+		return totalApples;
+	};
+	
+	Fruit oranges = (int totalOranges) -> {
+		if (totalOranges > 0) {
+			if (totalOranges % 3 == 0) {
+				totalOranges = (totalOranges / 3) + 1;
+			} else {
+				totalOranges = ((totalOranges / 3) * 2) + (totalOranges % 3);
+			}
+		}
+		return totalOranges;
+	};
+	
 
 	public double calulateTotaBill(List<String> listOfFruit) {
 		int totalApples = 0;
@@ -17,21 +40,7 @@ public class BillTotal {
 				totalOranges++;
 			}
 		}
-		if (totalApples > 0) {
-			if (totalApples % 2 == 0) {
-				totalApples = totalApples / 2;
-			} else {
-				totalApples = (totalApples / 2) + 1;
-			}
-		}
-		if (totalOranges > 0) {
-			if (totalOranges % 3 == 0) {
-				totalOranges = (totalOranges / 3) + 1;
-			} else {
-				totalOranges = ((totalOranges / 3) * 2) + (totalOranges % 3);
-			}
-		}
-		return (totalApples * applePrice) + (totalOranges * orangePrice);
+		return (apples.calculateDiscount(totalApples) * applePrice) + (oranges.calculateDiscount(totalOranges) * orangePrice);
 	}
 
 }
